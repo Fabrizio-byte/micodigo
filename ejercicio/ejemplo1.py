3e4c31a5ccfcf81198604ad0dcf0130cc1c5209a
DEBER <@Fabrizio-byte>
2024-08-29 19:32:28 -0500

    SUBE MI EJECERCICIO

---
+++ ejercicio/ejemplo1.py
@@ -0,0 +1,15 @@
+print("hola mundo ")
+# Carer una matriz de employee
+matriz= [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
+
+# Valor a int
+valor_a_int=5
+# int el valor
+def int_valor_en_matriz():
+ var =([[1, 2, 3],[4, 5, 6],[7, 8,
+                             9]])
+pass
+
+
